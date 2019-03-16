ee0f6d13b3ef85dac16d7db84f04dac11ca90f78
thaivanloi97 <47971240+thaivanloi97@users.noreply.github.com>
2019-03-16 15:47:31 +0700

    repair

---
+++ loi/add_product.php
@@ -0,0 +1,11 @@
+<?php>
+require_once("/entities/product.class.php);
+
+if(isset($_POST["btnsubmit"])){
+	$productName = $_POST["txtName"];
+	$$cateID=$_POST["txtCateID"];
+	$price=$_POST["txtprice"];
+	$quantity=$_POST["txtquantity"];
+	$description=$_POST["txtdesc"];
+	$picture=$_POST
+}
---
+++ loi/header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="vi">
+<head>
+	<meta charset="utf-8">
+	<meta name="author" content="nguyendinhanh"/>
+	<link href="/LAB3/site.css" rel="stylesheet"/>
+	<title>Project training - web ban hang</title>
+</head>
+<body>
+	<div id="wrapper">
+		<h2>Project training -  xay dung web ban hang</h2>
